b61a26b63b7fe18e3c51eae432be1c1214a86552
Proliecan <65495509+Proliecan@users.noreply.github.com>
2023-04-13 15:35:53 +0200

    Start renovating renderers

--- app/js/Renderers.js
+++ app/js/Renderers.js
@@ -2,11 +2,8 @@ const BaseRenderer = class BaseRenderer {
     constructor(canvas) {
         this.canvas = canvas.canvasObject;
         this.ctx = canvas.ctx;
-
-        // register renderer
-        Cycle.getInstance().registerRenderCallback(this.update.bind(this));
     }
-
+//EVERYTHING IN HERE SHOULD HAVE IT'S OWN RENDERER!!
     // fill with solid color
     fill(color) {
         this.ctx.fillStyle = color;
@@ -14,16 +11,16 @@ const BaseRenderer = class BaseRenderer {
     }
 
     // draw xy coordinates
-    drawCoordinates() {
+    drawCoordinatePattern(dist) {
         this.ctx.fillStyle = '#ffffff';
-        for (let x = 0; x < this.canvas.width; x += 100) {
-            for (let y = 0; y < this.canvas.height; y += 100) {
+        for (let x = 0; x < this.canvas.width; x += dist) {
+            for (let y = 0; y < this.canvas.height; y += dist) {
                 this.ctx.fillText(`(${x}, ${y})`, x, y);
             }
         }
     }
 
-    drawCircle(x, y, r, color = '#ffffff') {
+    drawCircle(x, y, r, color = '#000') {
         this.ctx.fillStyle = color;
         this.ctx.beginPath();
         this.ctx.arc(x, y, r, 0, 2 * Math.PI);
@@ -31,14 +28,14 @@ const BaseRenderer = class BaseRenderer {
     }
 
     // draw point
-    drawPoint(x, y, r, color = '#ffffff', label = '', labelColor = '#ffffff', labelFont = '5px Monospace') {
+    drawPoint(x, y, r, color = '#ff0000', label = '', labelColor = '#000', labelFont = '5px Monospace') {
         this.drawCircle(x, y, r, color);
         let fontHeight = parseInt(labelFont);
         this.drawString(label, x + r, y + r, labelColor, labelFont);
     }
 
     // draw string
-    drawString(string, x, y, color = '#ffffff', font = '20px Monospace') {
+    drawString(string, x, y, color = '#000', font = '20px Monospace') {
         this.ctx.fillStyle = color;
         this.ctx.font = font;
         // correct for font height
@@ -46,17 +43,26 @@ const BaseRenderer = class BaseRenderer {
         this.ctx.fillText(string, x, y);
     }
 
-    drawRectBorder(x, y, width, height, color = '#ffffff', lineWidth = 1) {
+    drawRectBorder(x, y, width, height, color = '#000', lineWidth = 1) {
         this.ctx.strokeStyle = color;
         this.ctx.lineWidth = lineWidth;
         this.ctx.strokeRect(x, y, width, height);
     }
+}
+
+const FramerateRenderer = class FramerateRenderer extends BaseRenderer {
+    constructor(canvas) {
+        super(canvas);
+    }
 
     //draw frame rate
-    drawFps(fps) {
+    drawFps(x, y, color = '#000', font = '20px Monospace') {
+        // get fps
+        let fps = Cycle.getInstance().fps;
+        
         // trim fps to 2 decimal places
         fps = fps.toFixed(2);
-
+        
         // fill before decimal with 0 to 3 digits
         for (let i = 1; i < 3; i++) { // don't fill 0.00
             let decimalFactor = Math.pow(10, i);
@@ -65,26 +71,8 @@ const BaseRenderer = class BaseRenderer {
             }
         }
 
-
         // draw fps
-        this.drawString(fps, 10, 10);
-    }
-
-    // update should be overwriten by child classes
-    update() {
-        // black background
-        this.fill('#000000');
-        //draw frame rate
-        this.drawFps(Cycle.getInstance().fps);
-
-        // draw warning
-        this.drawString('BaseRenderer.update() called. This should be overwritten by child classes.', 10, 30, '#ff0000', '20px Monospace');
-        // draw stack trace with line breaks
-        let stack = new Error().stack;
-        let lines = stack.split('\n');
-        lines.forEach((line, index) => {
-            this.drawString(line, 10, 50 + (20 * (index + 1)), '#ff0000', '20px Monospace');
-        });
+        this.drawString(fps, x, y, color, font);
     }
 }
 
@@ -124,11 +112,6 @@ const HexRenderer = class HexRenderer extends BaseRenderer {
         // calculate hex params
         let R = t;
         let r = 0.86602540378443864676372317075294 * R; // r = R * cos(30)
-
-        // // draw a single hex at x, y
-        // this.drawPoint(x, y, 2, '#ff0000', `C`, '#ff0000', '20px Monospace');
-        // this.drawPoint(x+r, y+R, 2, '#ff0000', `M`, '#ff0000', '20px Monospace');
-        // this.drawHex(x+r, y+R, R, r, color, lineWidth);
 
        // calculate hex grid params
         let hexWidth = 2 * r; 
@@ -186,17 +169,5 @@ const HexRenderer = class HexRenderer extends BaseRenderer {
     // fill canvas with hex pattern of given hex size
     fillCanvasWithHexPattern(hexSize, color = '#ffffff', lineWidth = 1) {
         this.fillRectWithHexPattern(0, 0, this.canvas.width, this.canvas.height, hexSize, color, lineWidth);
-    }
-
-
-    // update should be overwriten by child classes
-    update() {
-        // black background
-        this.fill('#000000');
-        //draw frame rate
-        this.drawFps(Cycle.getInstance().fps);
-
-        // fill canvas with hex pattern
-        this.fillCanvasWithHexPattern(50, '#ffffff', 1);
     }
 }
